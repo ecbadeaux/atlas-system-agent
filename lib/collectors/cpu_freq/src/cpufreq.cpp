8e37c6490ed723329f3628be4f012d7e962d3d83
Everett Badeaux <everettc1810@gmail.com>
2025-04-25 14:20:01 -0500

    fix cpufreq

---
+++ lib/collectors/cpu_freq/src/cpufreq.cpp
@@ -0,0 +1,32 @@
+#include "cpufreq.h"
+
+namespace atlasagent {
+template class CpuFreq<atlasagent::TaggingRegistry>;
+template class CpuFreq<spectator::TestRegistry>;
+
+template <typename Reg>
+void CpuFreq<Reg>::Stats() noexcept {
+  if (!enabled_) return;
+
+  DirHandle dh{path_prefix_.c_str()};
+
+  struct dirent* direntry;
+  // each logical cpu provides a directory with a name like policy%d
+  while ((direntry = readdir(dh)) != nullptr) {
+    if (direntry->d_name[0] == '.') continue;
+
+    auto prefix = fmt::format("{}/{}", path_prefix_, direntry->d_name);
+    auto min = static_cast<double>(read_num_from_file(prefix, "scaling_min_freq"));
+    if (min < 0) continue;
+    auto max = static_cast<double>(read_num_from_file(prefix, "scaling_max_freq"));
+    if (max < 0) continue;
+    auto cur = static_cast<double>(read_num_from_file(prefix, "scaling_cur_freq"));
+    if (cur < 0) continue;
+
+    min_ds_->Record(min);
+    max_ds_->Record(max);
+    cur_ds_->Record(cur);
+  }
+}
+
+}  // namespace atlasagent
--- lib/collectors/cpu_freq/src/cpufreq.h
+++ lib/collectors/cpu_freq/src/cpufreq.h
@@ -30,29 +30,7 @@ class CpuFreq {
         max_ds_{registry_->GetDistributionSummary("sys.maxCoreFrequency")},
         cur_ds_{registry_->GetDistributionSummary("sys.curCoreFrequency")} {}
 
-  void Stats() noexcept {
-    if (!enabled_) return;
-
-    DirHandle dh{path_prefix_.c_str()};
-
-    struct dirent* direntry;
-    // each logical cpu provides a directory with a name like policy%d
-    while ((direntry = readdir(dh)) != nullptr) {
-      if (direntry->d_name[0] == '.') continue;
-
-      auto prefix = fmt::format("{}/{}", path_prefix_, direntry->d_name);
-      auto min = static_cast<double>(read_num_from_file(prefix, "scaling_min_freq"));
-      if (min < 0) continue;
-      auto max = static_cast<double>(read_num_from_file(prefix, "scaling_max_freq"));
-      if (max < 0) continue;
-      auto cur = static_cast<double>(read_num_from_file(prefix, "scaling_cur_freq"));
-      if (cur < 0) continue;
-
-      min_ds_->Record(min);
-      max_ds_->Record(max);
-      cur_ds_->Record(cur);
-    }
-  }
+  void Stats() noexcept;
 
  private:
   Reg* registry_;
